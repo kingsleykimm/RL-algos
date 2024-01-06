0647dff16de53493fd3c4a35383036e0d83ee905
Kingsley Kim <kingsleykimm@gmail.com>
2024-01-06 02:00:31 -0500

    Finished up DQN and added a tentative double dqn

---
+++ agents/DDQN_Agent.py
@@ -0,0 +1,24 @@
+# Double Q-Learning Agent
+from .DQNAgent import DQNAgent
+from ..networks.dqn import DQN
+from ..utils import *
+import random
+import torch
+class DDQNAgent(DQNAgent):
+    def __init__(self):
+        super().__init__(self)
+    def get_batch_loss(self, batch):
+        for experience in batch:
+            target = torch.Tensor()
+            q = torch.Tensor()
+            for experience in batch:
+                s_j, action_j, reward_j, s_j_1, end = experience    
+                if end:
+                    target.add(reward_j)
+                else:
+                    with torch.no_grad():
+                        val = reward_j + self.discount_factor * self.network(s_j_1)[torch.argmax(self.target_network(s_j_1))]
+                q.add(self.network(s_j)[action_j])
+        loss = (target - q).square().mean()
+        return loss
+        
--- agents/DQNAgent.py
+++ agents/DQNAgent.py
@@ -13,9 +13,11 @@ class DQNAgent():
         self.replay_buffer = ReplayBuffer(self.config.get_param("replay_size"))
         self.env = AtariEnv(self.config.get_param('game_name'))
         self.optimizer = torch.optim.RMSprop(self.network.parameters(), lr=2.5e-4, momentum=0.95, eps=0.01, alpha=0.95)
+        self.param_updates = 0
     def run(self):
         M = self.config.get_param('episode_steps')
         T = self.config.get_param('max_time')
+        network_update = self.config.get_param('network_update_freq')
         initial_e = self.config.get_param('inital_e')
         final_e = self.config.get_param('final_e')
         discount_factor = self.config.get_param('discount_factor')
@@ -40,21 +42,30 @@ class DQNAgent():
                 self.replay_buffer.add_experience((cur_state, action, reward, obsv, term or trunc))
                 cur_state = obsv
                 # sample minibatch, but only after replay_start_size is finished
-                minibatch = self.replay_buffer.sample(batch_size)
-                for experience in minibatch:
-                    s_j, action_j, reward_j, s_j_1, end = experience    
-                    target = None
-                    if end:
-                        target = reward
-                    else:
-                        target = reward + discount_factor * max(self.target_network(s_j_1))
-                    # run the loss through target
-                    loss = (target - self.network(s_j)[action]) ** 2
-                    loss.backward()
-                    self.optimizer.zero_grad()
-                    self.optimizer.step()
+                if time_step % 4 == 0: #update frequency
 
-    
+                    minibatch = self.replay_buffer.sample(batch_size)
+                    loss = self.get_batch_loss(minibatch)
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+                    self.param_updates += 1
+                    if self.param_updates % network_update == 0: #every C steps, updates the network
+                        self.target_network.load_state_dict(self.network.state_dict())
+    def get_batch_loss(self, batch):
+        target = torch.Tensor()
+        q = torch.Tensor()
+        for experience in batch:
+            s_j, action_j, reward_j, s_j_1, end = experience    
+            if end:
+                target.add(reward_j)
+            else:
+                with torch.no_grad():
+                    target.add(reward_j + self.discount_factor * max(self.target_network(s_j_1)))
+            q.add(self.network(s_j)[action_j])
+        loss = (target - q).square().mean()
+        return loss
+
 
     def populate_memory(self):
         start_size = self.config.get_param('replay_start_size')
@@ -67,5 +78,3 @@ class DQNAgent():
             cur_state = obsv
             if term or trunc:
                 cur_state, info = self.env.reset()
-
-                
